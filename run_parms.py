9543cb584f25dd3ffd85dc6e2790b4c278abad9b
João Inácio <j.g.c.inacio@fys.uio.no>
2023-04-26 11:27:17 +0200

    ran code

--- run_parms.py
+++ run_parms.py
@@ -4,14 +4,14 @@ import numpy as np
 "0.5 1.0 1.0 0 0 0 "
 specs = lambda S, h, delta: f"{S} {delta} {h} 0 0 0"
 
-S = 0.5
-delta = 1.0
+S = 1.5
 
-h_vals_1 = np.arange(0.0, S*1.0, S*0.025)
-h_vals_2 = np.arange(S*1.0, S*3.0, S*0.25)
-h_vals_3 = np.arange(S*3.0, S*4.0, S*0.025)
+# delta = 1.0
+# h_vals = np.arange(0, S*4.0 + S*0.25, S*0.25)
 
-h_vals = np.concatenate([h_vals_1, h_vals_2, h_vals_3, [S*4.0]])
+h = 0.0
+delta_vals = np.arange(-2.0, 2.0+0.25, 0.25)
 
-for i, h in enumerate(h_vals):
-    subprocess.call(f"python3 exact_xxz_chain.py {specs(S, np.round(h, 6), delta)}", shell=True)
+# for i, h in enumerate(h_vals):
+for i, delta in enumerate(delta_vals):
+    subprocess.call(f"python3 exact_xxz_chain.py {specs(S, np.round(h, 6), np.round(delta, 6))}", shell=True)
